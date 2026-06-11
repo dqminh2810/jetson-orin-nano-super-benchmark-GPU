b7a145b98ce4f19099b2dce21f9ea32f78d25c5c
dqminh2810 <dqminh2810@gmail.com>
2026-06-11 21:20:37 +0700

    update

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## REQUIREMENTS
 
 *Hardware*
-- Nvidia Jetson Orin Nano - 8GB RAM
+- Nvidia Jetson Orin Nano Super
 - SSD NVME 128GB
 
 *Software*
@@ -16,6 +16,19 @@
 - Python 3.10.12
 
 ## BENCHMARK
-`. ./run.sh`
+#### cutclass_profiler
+`. ./run_cutclass_profiler.sh`
 
+*RESULT*
+`Sparse GEMM + INT8` & `m=512 n=512 k=8192` give `32.142 TOPS` about `50% SOL`
+
+`Sparse GEMM + FLOAT16` & `m=512 n=512 k=8192` give `19.283 TFLOPS` 
+
+*EXPECTED*
 ![performance](./img/perf_comparisons.png)
+
+#### ollama gemma3:4B
+you can run ollama model on different PCs to compare the performance
+
+`. ./run_ollama_gemma3-4b.sh`
+
---
+++ run_ollama_gemma3-4b.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+## ENABLE GPU FOR DOCKER
+#### Verify WSL2 Has GPU Access
+nvidia-smi
+
+#### Verify Container Runtime Integration
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+
+
+## INSTALL OLLAMA
+#### Natively install on MAC PRO
+#curl -fsSL https://ollama.com/install.sh | sh
+
+#### Standard without GPU
+#docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
+#### x86/Standard Systems with GPU
+#docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
+#### ARM-based NVIDIA Jetson environments
+docker run -d --runtime nvidia -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
+
+## INSTALL GEMMA3
+docker exec -it ollama ollama run --verbose gemma3
+
+#### Natively run on MAC PRO
+#ollama run --verbose gemma3
+
+
+## PROMPT
+# What's the derivative of f(x) = x^2 * exp(x) * sqrt(x)?
